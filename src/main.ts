ee02e8ddae33c96faa816b8e391a8d2d4ff53081
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-05-11 20:13:45 -0300

    chore: fix port docker and env

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   app.useGlobalInterceptors(new DatabaseInterceptor());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
